81630aa3970c5b1e21384a4cc54857509f561400
Danail <DanailMinchev@users.noreply.github.com>
2021-06-06 12:55:40 +0100

    fix(deps): upgrade Jest and other dependencies

--- jest.visual.config.js
+++ jest.visual.config.js
@@ -2,6 +2,8 @@ const jestConfig = require(`./jest.config`)
 
 const testMatch = `**/__visual_tests__/**/*.[jt]s?(x)`
 
+delete jestConfig.testEnvironment
+
 module.exports = {
   ...jestConfig,
   testPathIgnorePatterns: jestConfig.testPathIgnorePatterns.filter(
